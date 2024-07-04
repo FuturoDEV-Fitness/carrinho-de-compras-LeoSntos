78602d35d6a9811ee21a48114605b96340459d6b
LeoSntos <leobrognoli38@gmail.com>
2024-07-04 18:44:20 -0300

    Ex. 05 resolvido (cadastro de carrinho) Bonus:(listagem de carrinho)

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const clientsRoutes = require("./src/routes/clients.routes");
 const productsRoutes = require("./src/routes/products.routes");
+const ordersRoutes = require("./src/routes/orders.routes");
 
 const app = express()
 app.use(express.json());
 
 app.use("/clients", clientsRoutes);
 app.use("/products", productsRoutes);
+app.use("/orders", ordersRoutes);
 
 app.listen(3000, () => {
     console.log("SERVIDOR ONLINE NA PORTA 3000");
