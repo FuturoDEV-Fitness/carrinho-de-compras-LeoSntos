80f6b5e21ab53c7ba8ab1738ab8103df29128eaa
LeoSntos <leobrognoli38@gmail.com>
2024-07-04 19:08:19 -0300

    Adicionando comentarios

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json());
 
 app.use("/clients", clientsRoutes);
-app.use("/products", productsRoutes);
+app.use("/products", productsRoutes);   // rotas 
 app.use("/orders", ordersRoutes);
 
 app.listen(3000, () => {
